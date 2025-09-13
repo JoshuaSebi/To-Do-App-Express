e12b4777e6f4714483f871d02d6a846554aa85fd
Joshua <joshua.palokaran@gmail.com>
2025-09-13 15:06:09 +0530

    First commit

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 4040;
+app.set("view engine", "ejs");
 
+app.set("views", "./views");
 
+app.get('/', (req, res)=>{
+    res.render('index');
+})
+
+app.get('/notes', (req,res)=> {
+    res.render('notes',{
+        theTitle: "My Notes",
+        notes: notes
+    });
+})
+
+app.get('/notes/:id', (req,res)=>{
+    const id = +req.params.id;
+    const note = notes.find(n => n.id == id);
+
+});
+
+const notes = [
+    {id: 1, title: "Note 1", content: "This is note 1"},
+    {id: 2, title: "Note 2", content: "This is note 2"},
+    {id: 3, title: "Note 3", content: "This is note 3"},
+];
 
 app.use(express.static('public'));
 
